7119440b4972d55bf57c0b3e1e49a549479b4c61
Alex Griffin <a@ajgrf.com>
2018-03-23 16:02:00 -0500

    Use more human-centric date format

--- clock.go
+++ clock.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 func Clock(ch chan<- string) {
 	for {
-		ch <- time.Now().Format("2006-01-02 15:04")
+		now := time.Now()
+		ch <- fmt.Sprintf("%s %.1s %5s", now.Format("1/2"), now.Format("Mon"),
+			now.Format("3:04"))
 		offset := time.Duration(time.Now().UnixNano()) % time.Minute
 		time.Sleep(time.Minute - offset)
 	}
