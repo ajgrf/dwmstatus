b46d2368714bd19403405723cbe134c71d476292
Alex Griffin <a@ajgrf.com>
2018-03-23 22:21:05 -0500

    Add battery display

---
+++ battery_linux.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"time"
+)
+
+func Battery(bat string) func(chan<- string) {
+	return func(ch chan<- string) {
+		for {
+			b, err := ioutil.ReadFile("/sys/class/power_supply/" + bat +
+				"/capacity")
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			capacity, err := strconv.Atoi(string(bytes.TrimSpace(b)))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			if capacity < 98 {
+				ch <- fmt.Sprintf("B:%3d%%", capacity)
+			} else {
+				ch <- ""
+			}
+			time.Sleep(60 * time.Second)
+		}
+	}
+}
